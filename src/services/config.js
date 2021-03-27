a7c1e927ce895ba949e791e65069c99c4ffed7d1
Alejandra Miranda <ale.miranda890@gmail.com>
2021-03-27 15:18:11 -0400

    Règles de sécurité Firestore +ajout objet de conf

--- src/services/config.js
+++ src/services/config.js
@@ -5,7 +5,13 @@
 
 // Configuration Firebase (mettez-y les vôtres !)
 const firebaseConfig = {
-  
+  apiKey: "AIzaSyBXrvPDiqr6jXmeMFe9HQNR3m5mXpGFhzw",
+  authDomain: "pvt-h21-29626.firebaseapp.com",
+  projectId: "pvt-h21-29626",
+  storageBucket: "pvt-h21-29626.appspot.com",
+  messagingSenderId: "198365929320",
+  appId: "1:198365929320:web:cc4e72b2cb75658494c0dd",
+  measurementId: "G-HYXK18DVNT"
 };
 export default firebaseConfig;
 
